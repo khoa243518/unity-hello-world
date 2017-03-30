c76ebbb9c80ebe15058dfcd3de232650c4e07cad
khoa243518 <khoa243518@gmail.com>
2017-03-30 09:49:27 +0700

    Update MyBuilder.cs

--- Assets/Scripts/MyBuilder.cs
+++ Assets/Scripts/MyBuilder.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -60,3 +61,4 @@ public class MyBuilder : MonoBehaviour {
 			// Then as with the Device build.
 	}
 }
+#endif
